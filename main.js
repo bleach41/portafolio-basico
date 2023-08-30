5164ff04ba4c80b94026c185e0e7c111738e7be0
Tony Alejandro <ftonyalejandrofr@gmail.com>
2023-08-30 03:34:38 -0400

    2.0 falta los link y servicio de contacto

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
+let menuVisible = false
+function mostrarOcultarMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = ""
+    menuVisible = false
+  }
+  else {
+    document.getElementById("nav").classList = "responsive"
+    menuVisible = true
+  }
+}
 
+function seleccionar() {
+  document.getElementById("nav").classList = ""
+  menuVisible = false
 
-document.querySelector('#app').innerHTML = `
-  <div>
+}
 
-  </div>
-`
+function efectoHabilidades() {
+  var habilidades = document.getElementsByClassName("progreso");
+
+  for (var i = 0; i < habilidades.length; i++) {
+    var habilidad = habilidades[i];
+    var distancia_habilidad = window.innerHeight - habilidad.getBoundingClientRect().top;
+
+    if (distancia_habilidad >= 300) {
+      habilidad.classList.add(habilidad.dataset.animation);
+    }
+  }
+}
+
+window.onscroll = function () {
+  efectoHabilidades();
+}
+// document.querySelector('#app').innerHTML = `
+//   <div>
+
+//   </div>
+// `
 
